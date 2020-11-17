b22435ebfaf9f7e29c5254c52a0a98af6b3a0ccf
Aubrey Yates <aubreyyates@futurewebservice.com>
2020-11-16 19:37:32 -0600

    Add item creator function

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# item-creator
+This package provides a function to help you create classes.
+
+For example:
+
+Let us say we want to create a class called "User".
+
+It will get have 3 parameters we want passed to it when an instance of the class is created. These will be id, name, and age.
+
+Getters and setters are created for all of the parameters that are put into classParameters.
+
+```js
+var itemCreator = require('item-creator');
+
+// Set globalObject to window if in a browser.
+itemCreator.itemCreator({ globalObject: global, className: 'User', classParameters: ['id', 'name', 'age'] });
+
+var user1 = new User(1, 'Jimmy', 34);
+
+console.log(user1.getName()); // Prints: Jimmy
+console.log(user1.getAge()); // Prints: 34
+
+user1.setName('Jake');
+console.log(user1.getName()); // Prints: Jake
+```
+
+You can also add functions to the class through classFunctions.
+
+For example:
+
+```js
+var itemCreator = require('item-creator');
+
+function printHello() {
+    console.log('Hello!');
+}
+
+itemCreator.itemCreator({ globalObject: global, className: 'User', classParameters: ['id', 'name', 'age'], classFunctions: [printHello] });
+
+var user1 = new User(1, 'Ashley', 26);
+
+user1.printHello(); // Prints: Hello!
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+function itemCreator({ globalObject, className, classParameters = [], classFunctions = [] } = {}) {
+
+    if (typeof className != 'string') return console.log("You need a string for className. No class created.");
+    if (!(classParameters instanceof Array)) return console.log("You need an array for classParameters. No class created.");
+    if (!(classParameters instanceof Array)) return console.log("You need an array for classFunctions. No class created.");
+
+    let parameters = '';
+    let constructor = '';
+
+    // Set up the string for the parameters and contructor.
+    classParameters.forEach((parameter, i) => {
+        if (i < (classParameters.length - 1)) {
+            parameters += (parameter + ', ');
+        } else {
+            parameters += parameter;
+        }
+
+        constructor += `this.${parameter}=${parameter};`;
+    });
+
+    // Create the class.
+    let classCreator = new Function(`return function(${parameters}) {${constructor}}`)();
+    globalObject[className] = classCreator;
+
+    // Create setters and getters for each parameter.
+    classParameters.forEach(parameter => {
+        let param = parameter.charAt(0).toUpperCase() + parameter.slice(1);
+
+        globalObject[className].prototype["set" + param] = function (setter) {
+            this[parameter] = setter;
+        }
+
+        globalObject[className].prototype["get" + param] = function () {
+            return this[parameter];
+        }
+    });
+
+    // Create method named getClassName to return the name of the class.
+    globalObject[className].prototype["getClassName"] = function () {
+        return className;
+    };
+
+    // Add classFunctions to the class to make methods.
+    classFunctions.forEach(classFunction => {
+        globalObject[className].prototype[classFunction.name] = classFunction;
+    });
+
+}
+
+if (typeof module !== 'undefined') module.exports.itemCreator = itemCreator;
